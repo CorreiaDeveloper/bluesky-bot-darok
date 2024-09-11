64c1abb674d226249c3e628698335104a706a784
Gabriel Correia <gabrielcorreiajobs@hotmail.com>
2024-09-10 21:25:28 -0300

    Implementado gerenciamento de filas para processar curtidas com um intervalo configurável pelo usuário, medido em segundos, entre as ações. Isso torna as curtidas mais naturais e evita que posts recentes sejam curtidos imediatamente após sua publicação.

--- src/config/config.js
+++ src/config/config.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const API_SERVICE_URL = 'https://bsky.social';
-export const LIKE_INTERVAL = 15 //SEGUNDOS ENTRE OS LIKES
+export const LIKE_INTERVAL = 15 //FORTEMENTE SUGERIDO A DEIXAR MAIOR OU IGUAL A 15
 export const LOGS = false;
